56cdd9f963ba25c67e45d71e5615df23d9d87378
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2023-01-12 12:13:35 -0300

    criando estilizações separadas para os arquivos.

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View,Text } from "react-native";
+import styles from "./style";
 
 export default function Title(){
     return(
-        <View>
-            <Text>Teste de App 1.0</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>Teste de App 1.0</Text>
         </View>
     )
 }
